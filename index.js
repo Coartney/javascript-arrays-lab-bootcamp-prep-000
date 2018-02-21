bd4fd5d60b6c4f66bad79cb344069c884ed52ae2
coartney22@gmail.com <coartney22@gmail.com>
2018-02-21 16:56:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,13 +13,15 @@ function destructivelyRemoveFirstKitten(){
   return kittens.shift();
 }
 function appendKitten(name){
-  return kittens.concat(name)
+  return kittens.concat(name);
 }
 function prependKitten(name){
   var prependKitten = [name, ...kittens];
-  return prependKitten
+  return prependKitten;
 }
 function removeLastKitten(){
-  return kittens.slice(0, kittens.length - 1)
-
+  return kittens.slice(0, kittens.length - 1);
+}
+function removeFirstKitten(){
+  return kittens.slice(1);
 }
