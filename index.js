475c250aa26e4e076a6afe5dcd5d6d6b6f04e48a
coartney22@gmail.com <coartney22@gmail.com>
2018-02-21 16:52:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function prependKitten(name){
   return prependKitten
 }
 function removeLastKitten(){
-  var removeLastKitten = kittens.slice(-1)
+  var removeLastKitten = kittens.slice(1)
   return removeLastKitten
 }
